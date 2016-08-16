c23a407862da97945fbe20442690c1939d6be8b4
Zach Wernberg <zawerny@gmail.com>
2016-08-16 07:02:50 -0500

    export take 2

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 import { ToolbarComponent } from './toolbar/index';
 import { NavbarComponent } from './navbar/index';
 import { NameListService } from './name-list/index';
-import { Player,PlayerService } from './player/index';
+import { PlayerService } from './player/index';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -15,14 +15,14 @@ import { Player,PlayerService } from './player/index';
 @NgModule({
   imports: [CommonModule, RouterModule],
   declarations: [ToolbarComponent, NavbarComponent],
-  exports: [ToolbarComponent, NavbarComponent, Player,PlayerService,
+  exports: [ToolbarComponent, NavbarComponent,
     CommonModule, FormsModule, RouterModule]
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [NameListService]
+      providers: [NameListService, PlayerService]
     };
   }
 }
